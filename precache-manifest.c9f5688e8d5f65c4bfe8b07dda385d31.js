0b34c17c58ec1de0db9bd852eec2617e19d3e0cc
MedBenHadid <benhdid21@gmail.com>
2019-11-20 21:01:03 +0100

    Updates

---
+++ precache-manifest.c9f5688e8d5f65c4bfe8b07dda385d31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436f4bc1d3194b58c2c06723d6cb2924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be65b36181a942086254",
+    "url": "/static/css/main.46714e64.chunk.css"
+  },
+  {
+    "revision": "39e2432d2996fc11a812",
+    "url": "/static/js/2.c7ae1e47.chunk.js"
+  },
+  {
+    "revision": "be65b36181a942086254",
+    "url": "/static/js/main.c15dd050.chunk.js"
+  },
+  {
+    "revision": "c0d1ba1ba96f0d6c47d2",
+    "url": "/static/js/runtime-main.a5507d4d.js"
+  },
+  {
+    "revision": "28b7c6579952877724ee9e254ff8ea63",
+    "url": "/static/media/1.28b7c657.png"
+  },
+  {
+    "revision": "87c05913fbeb18f3d4c1e57dff92627b",
+    "url": "/static/media/2.87c05913.png"
+  },
+  {
+    "revision": "0634109700548d979a4b6c108b8d660a",
+    "url": "/static/media/3.06341097.png"
+  },
+  {
+    "revision": "eca0cb92e595d9083c45cfd292b676ae",
+    "url": "/static/media/4.eca0cb92.png"
+  },
+  {
+    "revision": "93801a17c3962576976bd954d75dd01e",
+    "url": "/static/media/Artisan.93801a17.png"
+  },
+  {
+    "revision": "0013358e836e8e0ddeb8ba7455d04f2f",
+    "url": "/static/media/logo.0013358e.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
